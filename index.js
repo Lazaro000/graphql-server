39c78c359ea734f2a2ab4faed4c1c7adf81421b3
Lázaro <lazaroyaesta@gmail.com>
2022-07-22 23:47:41 +0200

    🎉 Creating GraphQL server with Apollo

---
+++ index.js
@@ -0,0 +1,22 @@
+const persons = [
+  {
+    name: "Midu",
+    phone: "034-1234567",
+    street: "Calle Frontend",
+    city: "Barcelona",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Youseff",
+    phone: "044-123456",
+    street: "Avenida Fullstack",
+    city: "Mataro",
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Itzi",
+    street: "Pasaje Testing",
+    city: "Ibiza",
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
+  },
+];
